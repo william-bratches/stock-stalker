ad0020f34e451b54f82a65c6e01aba5966ebb747
william-bratches <william.bratches@gmail.com>
2018-07-01 12:20:15 -0400

    go to page

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ WIP. An experiment with pupeteer and pattern recognition...
 
 // start by getting the reporter working
 // is there a free stock trading api?
+
+* TODO
+* dynamic player selection (or, input player)
--- src/report.js
+++ src/report.js
@@ -3,8 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com');
+  await page.goto('https://www.marketwatch.com/game/official-reddit-challenge-2018');
+  const url = await page.evaluate(() => $('tr td')[1].querySelector('a').href);
+  await page.goto(url);
   await page.screenshot({path: 'bin/screenshots/example.png'});
-
   await browser.close();
 })();
