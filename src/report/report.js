bccca5d28f6d584ba76157060a6af03776dd0fbc
william-bratches <william.bratches@gmail.com>
2018-07-07 18:15:17 -0400

    commit before plan b: physically click link

--- src/report/report.js
+++ src/report/report.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
-const GAME_NAME = 'official-reddit-challenge-2018'
+
+const GAME_NAME = 'official-reddit-challenge-2018';
 
 // should I get by top performance overall, or fastest growing player?
 const getTopPlayerUrl = async () => {
@@ -11,40 +12,37 @@ const getTopPlayerUrl = async () => {
   return url;
 };
 
-const getPlayerIdFromUrl = (url) => {
-  const parsedUrl = new URL(url);
-  return parsedUrl.searchParams.get('p');
-}
+// const getPlayerIdFromUrl = (url) => {
+//   const parsedUrl = new URL(url);
+//   return parsedUrl.searchParams.get('p');
+// };
+
 
 const getPlayerReport = async (url) => {
   console.log('fetching player report...');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const playerId = getPlayerIdFromUrl(url);
-  console.log(playerId);
-  const csvUrl = `https://www.marketwatch.com/game/${GAME_NAME}/download?view=transactions&amp;count=16&amp;p=${playerId}`;
+  // const playerId = getPlayerIdFromUrl(url);
+  // const csvUrl = `https://www.marketwatch.com/game/${GAME_NAME}/download?view=transactions&amp;count=16&amp;p=${playerId}`;
   const file = await page.evaluate(() => {
-    return fetch(csvUrl, { credentials: 'same-origin', responseType: 'arraybuffer' })
-      .then(response => response.arrayBuffer())
-      .then(arrayBuffer => {
-        console.log(arrayBuffer);
-        const bufString = arrayBufferToString(arrayBuffer);
-        console.log(bufString);
-        return window.writeABString(bufstring, '/tmp/downloadtest.pdf');
-      });
- });
- return file;
-}
+    const tempHardCodedUrl = 'https://www.marketwatch.com/game/official-reddit-challenge-2018/download?view=transactions&amp;count=16&amp;p=2349626';
+    return fetch(tempHardCodedUrl, { credentials: 'same-origin', responseType: 'arraybuffer' })
+      .then(res => res.json())
+      .then(console.log)
+      .catch(console.log);
+  });
+  return file;
+};
 
-const watchPlayer = (url) => {
-  setInterval(async () => {
-    const file = await getPlayerReport(url);
-    // if timestamp is greater
-    // go wild, otherwise pass
-  }, 80000);
-  // every 80 seconds, get new copy of report
-  // check to see if any changes have been made
-}
+// const watchPlayer = (url) => {
+//   setInterval(async () => {
+//     const file = await getPlayerReport(url);
+//     // if timestamp is greater
+//     // go wild, otherwise pass
+//   }, 80000);
+//   // every 80 seconds, get new copy of report
+//   // check to see if any changes have been made
+// }
 
 module.exports = {
   getTopPlayerUrl,
