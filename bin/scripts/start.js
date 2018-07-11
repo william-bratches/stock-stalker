fe4cfeeb5fd0d52fec824e48ac47091254256c60
william-bratches <william.bratches@gmail.com>
2018-07-10 23:23:01 -0400

    wip getting mongo to work correctly

--- bin/scripts/start.js
+++ bin/scripts/start.js
@@ -3,17 +3,17 @@ const path = require('path');
 
 const defaultCb = () => {};
 
-const makeDataDir = (cb = defaultCb) => {
-  return spawn('mkdir', ['/data']).on('close', cb);
-};
-
 const startMongo = (cb = defaultCb) => {
-  return spawn('mongod', ['--dbpath=./bin/db'], {
-    stdio: 'inherit',
-  }).on('data', cb);
+  const mongo = spawn('mongod', ['--dbpath=./bin/db'], { stdio: 'inherit' });
+
+  mongo.stdout.on('data', (data) => {
+    // if waiting for connections magic toString
+    // start server
+  });
 };
 
 const startServer = (cb = defaultCb) => {
+  console.log('starting server...');
   const context = path.resolve(__dirname, '../../src');
   return spawn('node', ['index.js'], {
     stdio: 'inherit',
@@ -21,4 +21,4 @@ const startServer = (cb = defaultCb) => {
   }).on('data', cb);
 };
 
-makeDataDir(() => startMongo(() => startServer));
+startMongo(() => startServer());
