da2bbc5318fe3e2de6b20ab20b21efe66bc51fc7
william-bratches <william.bratches@gmail.com>
2018-07-15 12:04:33 -0400

    add safe shutdown script

--- bin/scripts/shutdown.js
+++ bin/scripts/shutdown.js
@@ -1,1 +1,9 @@
-// mongo --eval "db.getSiblingDB('admin').shutdownServer()"
+const { spawn } = require('child_process');
+
+const ADMIN = 'admin';
+
+const stopMongo = () => {
+  return spawn(`mongo --eval "db.getSiblingDB(${ADMIN}).shutdownServer()"`, { shell: true });
+};
+
+stopMongo();
