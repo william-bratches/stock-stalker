f335b57ea81679e9c965c2316849a16a263f627f
william-bratches <william.bratches@gmail.com>
2018-07-26 16:02:52 -0400

    wip debugging

--- src/report/engine.js
+++ src/report/engine.js
@@ -6,13 +6,17 @@ const { getPlayerIdFromUrl } = require('../lib/parsing');
 const WATCH_INTERVAL = 30000;
 
 const getLatestHistory = async (collection, url) => {
+  console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$');
   const playerId = getPlayerIdFromUrl(url);
-  await collection.find(playerId);
+  console.log(playerId);
+  const latestRecord = await collection.find(playerId);
+  console.log(latestRecord);
+  return latestRecord;
 };
 
 const determineIfChanged = (data, lastDocument) => {
   const newTradeHash = hashTrade(data[0]);
-  const oldTradeHash = lastDocument.hash;
+  const oldTradeHash = lastDocument.hash; // TODO: probably bc it doesn't exist!
   return newTradeHash !== oldTradeHash;
 };
 
@@ -35,34 +39,40 @@ const alertBroker = (trades) => {
   // twilio integration?
 };
 
-const watchPlayer = (url, db) => {
-  setInterval(async () => {
-    console.log('***************************');
-    console.log('triggering watch sequence!')
-    const data = await getPlayerReport(url);
-    console.log(data);
-    console.log('--------------')
-    const collection = transactionHistory(db);
-    console.log(collection);
-    console.log('--------------')
-    const oldHistory = await getLatestHistory(collection, url);
-    console.log(oldHistory);
-    console.log('--------------')
-    const hasChanged = determineIfChanged(data, oldHistory);
-    console.log(hasChanged);
+const mainSequence = async (url, db) => {
+  console.log('triggering watch sequence!');
+  const data = await getPlayerReport(url);
+  const collection = transactionHistory(db);
+  const oldHistory = await getLatestHistory(collection, url);
+  console.log('******************');
+  console.log(oldHistory);
+  console.log('--------------');
+  if (!oldHistory) {
+    updateHistory(collection, data, url);
+    return;
+  }
 
-    if (hasChanged) {
-      const newHistory = await updateHistory(collection, data, url);
-      const newTrades = await diff(oldHistory, newHistory);
-      await alertBroker(newTrades);
-    }
-  }, WATCH_INTERVAL);
+  const hasChanged = determineIfChanged(data, oldHistory);
+  console.log(hasChanged);
+
+  if (hasChanged) {
+    const newHistory = await updateHistory(collection, data, url);
+    const newTrades = await diff(oldHistory, newHistory);
+    await alertBroker(newTrades);
+  }
 };
+
+// const watchPlayer = (url, db) => {
+//   setInterval(async () => {
+//     mainSequence();
+//   }, WATCH_INTERVAL);
+// };
 
 const startReporting = (db) => {
   // for now, just a hardcoded url for testing. I'll figure out dynamic stuff later
   const tempHardcodedUrl = 'http://www.marketwatch.com/game/official-reddit-challenge-2018/portfolio?p=2349626&name=Notice%20Me%20Sempai';
-  watchPlayer(tempHardcodedUrl, db);
+  // watchPlayer(tempHardcodedUrl, db);
+  mainSequence(tempHardcodedUrl, db);
 };
 
 module.exports = startReporting;
