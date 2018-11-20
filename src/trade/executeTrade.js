135370d27efbfafc70760f1c060f6071ce3420e5
william-bratches <william.bratches@gmail.com>
2018-11-19 19:48:35 -0500

    notes

--- src/trade/executeTrade.js
+++ src/trade/executeTrade.js
@@ -5,8 +5,16 @@ const orderTypeMap = {
   Short: 'order-short',
 }
 
+const calculateSharesToTrade = async (trade, networths) => {
+  // if buy or short order,  calculate based on net worth
+  // if sell, just sell all of it
+  const volumeAsNumber = Number(trade.volume.replace(/[^0-9.-]+/g, ''));
+  const traderNetWorthAsNumber = Number(traderNetWorth.replace(/[^0-9.-]+/g, ''));
+  const percentNetWorth = volumeAsNumber / traderNetWorthAsNumber;
+};
 
-const executeTrade = async (browser, trade, currentNetWorth) => {
+
+const executeTrade = async (browser, trade, protagonistNetWorth, traderNetWorth) => {
   const page = await browser.newPage();
   await page.goto('https://www.marketwatch.com/game/official-reddit-challenge-2018');
   await page.type('.j-miniTrade', trade.symbol);
@@ -19,7 +27,10 @@ const executeTrade = async (browser, trade, currentNetWorth) => {
   // check if actually selected - if not, abandon
 
   // set type of trade - if none, just return and cancel this action
+
   // calculate amount to buy based on % of portfolio - need to add total portfolio value to trade information
+  // after calc, set shares and go
+  // what happens if sets more than can be afforded?
   // click submit order
 };
 
